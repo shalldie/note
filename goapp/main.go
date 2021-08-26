79b9b59a5b3fff6c37cec2bb00dce1a80d7920e3
shalldie <hi.xieshuang@gmail.com>
2021-08-26 16:20:21 +0800

    update

--- goapp/main.go
+++ goapp/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+// import "fmt"
+
+// func If(condition bool,trueResult,falseResult)  {
+// 	if condition{
+// 		return trueResult
+// 	}
+
+// 	return falseResult
+// }
+
+// func balabala(condition bool,tr,fr)  {
+// 	if condition{
+// 		return tr
+// 	}
+
+// 	return fr
+// }
 
 func main() {
-	fmt.Println("hello world")
+
+	response, err := http.Get("https://nosaid.com")
+
+	if err != nil {
+		println(err)
+		return
+	}
+
+	defer response.Body.Close()
+
+	result, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		return
+	}
+
+	println(string(result))
+
 }
