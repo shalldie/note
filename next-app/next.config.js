7aaf901690c9add52ee5567730b4254caab44b0a
shalldie <hi.xieshuang@gmail.com>
2023-02-25 02:03:49 +0800

    readd next.config.js

---
+++ next-app/next.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+
+module.exports = async (phase, {defaultConfig}) => {
+    /**
+     * @type {import('next').NextConfig}
+     */
+    const nextConfig = {
+        // experimental: {
+        //     appDir: true
+        // },
+        reactStrictMode: false,
+        env: {
+            CDN_PREFIX: process.env.CDN_PREFIX
+        },
+        sassOptions: {
+            prependData: [
+                //
+                `$CDN_PREFIX: '${process.env.CDN_PREFIX}';`,
+                `@import "~/assets/styles/variable.scss";`,
+                `@import "~/assets/styles/mixin.scss";`
+            ].join('\n')
+        },
+        async rewrites() {
+            return {
+                fallback: [
+                    {
+                        source: '/api/:path*',
+                        destination: `https://nosaid.com/api/:path*`
+                    },
+                    {
+                        source: '/static/:path*',
+                        destination: `https://nosaid.com/static/:path*`
+                    }
+                ]
+            };
+        }
+    };
+
+    // bundle 分析
+    if (process.env.ANALYZE) {
+        const withBundleAnalyzer = require('@next/bundle-analyzer')({
+            enabled: true
+        });
+
+        return withBundleAnalyzer(nextConfig);
+    }
+
+    return nextConfig;
+};
